4b41253b10ad9da44eac30fd45a7b192cdc7aeca
Chiamie <okechukwurchiamaka@gmail.com>
2026-02-18 05:01:22 +0100

    attempted dietel question 4.22

---
+++ tabular_output.go
@@ -0,0 +1,35 @@
+
+
+
+
+package main
+
+import "fmt"
+
+
+func main(){
+
+	fmt.Print("N\tN2\tN3\tN4")
+	for index := 1; index <= 5; index++ {
+		fmt.Printf("\n%d\t", index)
+		fmt.Printf("%d\t", index * index)
+		fmt.Printf("%d\t", index * index * index)
+		fmt.Printf("%d\t", index * index * index * index)
+		
+		
+	
+	
+	
+	
+	}
+
+		
+	
+
+}
+
+
+
+
+
+
